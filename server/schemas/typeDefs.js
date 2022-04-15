ced8678969d5a47c120831a2a40384674d2b423d
Danielle Fortin <fortind6@gmail.com>
2022-04-15 11:03:05 -0400

    added typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,44 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+
+    type Query {
+        me: User
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String
+        bookCount: Int
+        savedBooks: [Books]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId!): User
+    }
+
+`;
+
+
 
 // export the typeDefs
 module.exports = typeDefs;
